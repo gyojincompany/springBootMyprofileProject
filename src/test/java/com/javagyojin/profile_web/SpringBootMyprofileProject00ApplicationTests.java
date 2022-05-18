e52bce4b835f79ebf1bcf2f8ae6ada3f443002d5
gyojincompany <luckyred@gmail.com>
2022-05-18 15:05:17 +0900

    SpringBoot myprofile home project v1.0

---
+++ src/test/java/com/javagyojin/profile_web/SpringBootMyprofileProject00ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javagyojin.profile_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMyprofileProject00ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
